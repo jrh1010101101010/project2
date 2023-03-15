2be78d2e83384341a0dc157140fdaaf2840fb17d
jrh1010101101010 <jhamblin798@gmail.com>
2023-03-15 12:23:43 +1100

    fifth commit

--- controllers/workout_controllers.js
+++ controllers/workout_controllers.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const db = require('../db/index')
 const ensureLoggedIn = require('../middleware/ensure_logged_in')
 
-// db 
 // ensureloggedin function
 
 router.get('/', (req,res) =>{
@@ -16,11 +15,16 @@ router.get('/', (req,res) =>{
     
 })
 
-router.get('/workout/:id', ensureLoggedIn, (req, res) =>{
+// share new
+router.get('/workout/new', (req,res) =>{
+    res.render('new_workout')
+})
+// workout details
+router.get('/workout/:id',  (req, res) =>{
     const sql = `select * from workout where id = $1;`
 
     db.query(sql, [req.params.id], (err, dbRes) =>{
-        console.log(dbRes)
+        
         if (err){
             console.log(err)
         } else {
@@ -29,6 +33,57 @@ router.get('/workout/:id', ensureLoggedIn, (req, res) =>{
             
             res.render('workout_details', {workout})
         }   
+    })
+})
+
+// routing for new dishes
+router.post('/workout', (req,res) =>{
+    const sql = `insert into workout (title, description, set) values ($1, $2, $3);`
+    db.query(sql, [req.body.title, req.body.description, req.body.sets],(err,dbres)=>{
+        res.redirect('/')
+    })
+
+})
+
+// editing function 
+router.get('/workout/:id/edit', (req,res) =>{
+    const sql = `select * from workout where id = $1;`
+
+
+    db.query(sql, [req.params.id], (err, dbRes) =>{
+        
+        if (err){
+            console.log(err)
+        } else {
+            
+            const workout = dbRes.rows[0]
+            
+            res.render('edit_workout', {workout})
+        }   
+    })
+})
+// route to update the db and then send back to workout details
+router.put ('/workout/:id', (req,res) =>{
+    const sql = `UPDATE workout SET title = $1, description = $2, set = $3 WHERE id = $4;`
+    console.log(req.body)
+    db.query(sql, 
+        [req.body.title, req.body.description, req.body.set, req.params.id], 
+        (err, dbRes) =>{
+        
+        console.log(dbRes.rows)
+        if (err){
+            console.log(err)
+        } else {
+            res.redirect(`/workout/${req.params.id}`)
+        }
+    })
+})
+
+// delete function
+router.delete('/workout/:id', (req,res) =>{
+    const sql = `delete from workout where id = $1`
+    db.query (sql, [req.params.id], (err,dbRes) =>{
+        res.redirect('/')
     })
 })
 
--- db/db.sql
+++ db/db.sql
@@ -16,3 +16,7 @@ create table users (
 );
   
 insert into workout (title, description, set, user_id) values ('chest and shoulders', 'chest focused day with a focus on pressing to work on front delts with individual shoulder exercises for other shoulder heads', 'dumbell bench for 4 x 12, dumbell incline bench 4 x 8, Shoulder Shrugs 3 x 10, Lateral Raises 3 x 12, Reverse Cable Flys 3 x 12, Low Cable Flys 3 x 8, Chest Press 3 x 12', '3');
+
+insert into workout (title, description, set, user_id) values ('back', 'back day with a focus on high volume', 'Lat pulldowns 4x10+partials, t-bar rows 3x10 + drop set, 4 x midrows, Chest supported cable row 3x10, Rope pullodwn 3x12', '3');
+
+insert into workout (title, description, set) values ('', '', '');
